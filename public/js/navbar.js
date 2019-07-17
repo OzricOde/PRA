d8c467e9d4e380781f1dc7d707d37317c547b077
Zaid <zaidjan1295@gmail.com>
2019-07-17 09:09:08 +0530

    jazzed it up

---
+++ public/js/navbar.js
@@ -0,0 +1,12 @@
+document.getElementById("nav").innerHTML += `
+<nav class="navbar navbar-default">
+    <div class="container-fluid">
+        <div class="navbar-header">
+        <a class="navbar-brand" href="#">RGS</a>
+        </div>
+        <ul class="nav navbar-nav">
+        <li class="active"><a href="./addTemp.html">Add Template</a></li>
+        <li><a href="./viewTemp.html">View Template</a></li>
+        </ul>
+    </div>
+</nav>`
