8acf84f7d584976d2b6e2467d4510170cf2ef587
Zaid <zaidjan1295@gmail.com>
2019-07-25 20:48:59 +0530

    pre jenkins

---
+++ oct1.js
@@ -0,0 +1,32 @@
+    
+const Octokit = require('@octokit/rest')
+const octokit = new Octokit({
+  auth: 'token a571af79b98093e504a67da8bac840a748faa9ba'
+ })
+
+
+// octokit.repos.listForUser({
+//   username: 'vanesssapearlss',
+//   }).then(({ data }) => {
+//   console.log({data})
+// })
+
+
+
+// octokit.repos.createForAuthenticatedUser({
+//   name: 'bye',
+// })
+
+// octokit.repos.delete({
+//   owner : 'vanesssapearlss',
+//   repo : 'copy'
+// })
+
+
+octokit.repos.createOrUpdateFile({
+  owner : 'vanesssapearlss',
+  repo : 'aaa',
+  path : 'eee/rrr',
+  message: 'kjdkjdfb',
+  content : 'bXkgbmV3IGZpbGUgY29udGVudHM='
+})
